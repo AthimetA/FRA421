3cc6a273f1afe4fd1759f38084f58d72628389b4
Athimet Aiewcharoen <mobiltiy2604@gmail.com>
2022-09-08 10:43:41 +0700

    H745 init

--- l010/Core/Inc/main.h
+++ l010/Core/Inc/main.h
@@ -61,16 +61,28 @@ void Error_Handler(void);
 #define B1_GPIO_Port GPIOC
 #define MCO_Pin GPIO_PIN_0
 #define MCO_GPIO_Port GPIOH
+#define S1_Pin GPIO_PIN_0
+#define S1_GPIO_Port GPIOA
+#define S2_Pin GPIO_PIN_1
+#define S2_GPIO_Port GPIOA
 #define USART_TX_Pin GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
+#define S3_Pin GPIO_PIN_4
+#define S3_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
 #define TCK_GPIO_Port GPIOA
+#define LED1_Pin GPIO_PIN_3
+#define LED1_GPIO_Port GPIOB
+#define LED3_Pin GPIO_PIN_4
+#define LED3_GPIO_Port GPIOB
+#define LED2_Pin GPIO_PIN_5
+#define LED2_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
