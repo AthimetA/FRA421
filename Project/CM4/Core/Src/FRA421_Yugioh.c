fdeea69a3535579123ce5704efc006e272513dfe
Athimet Aiewcharoen <mobiltiy2604@gmail.com>
2022-12-30 22:02:51 +0700

    update

--- Project/CM4/Core/Src/FRA421_Yugioh.c
+++ Project/CM4/Core/Src/FRA421_Yugioh.c
@@ -37,13 +37,13 @@ void YUGIOH_card_register(RFIDHandle *rfidmain)
 
 	ptrRFID = &rfidmain->RFID[rfidmain->slaveNum];
 	ptrCard = &ptrRFID->detectedCard;
-	ptrYUGIOHCard = &ptrRFID->bufferCard;
+	ptrYUGIOHCard = ptrRFID->bufferCard;
 
 	// For now fix number
 	ptrYUGIOHCard->cardData = ptrCard->data;
 	ptrYUGIOHCard->cardSignature = 1 % 256;
-	ptrYUGIOHCard->cardState = 2 % 3;
-	ptrYUGIOHCard->cardType = 2 % 2;
+	ptrYUGIOHCard->cardState = 0 % 3;
+	ptrYUGIOHCard->cardType = 0 % 2;
 
 }
 
@@ -61,39 +61,53 @@ void YUGIOH_card_Management(RFIDHandle *rfidmain)
 	YUGIOH_Card *ptrYugiohCard_Main;
 	ptrYugiohCard_Main = &ptrRFID->mainCard;
 
-	/* Switch Action State
+	/*  Action State (Can not use Switch Case TT)
+	 * https://stackoverflow.com/questions/2308323/why-no-switch-on-pointers
 	 * 		- 0 = No Card
 	 * 		- 1 = Card in Play
 	 * 		- 2 = Card Played
+	 * 		- 255 = Action error clear all buffer
 	 *  */
-	YUGIOH_card_copy(ptrYugiohCard_Current, ptrYugiohCard_Main);
-	RFID_Clear_Card_Bufffer(ptrRFID);
-	ptrRFID->action = 1;
-//	switch (ptrRFID->action)
-//	{
-//		case 0:
-//			// Main Card is not assign
-//			YUGIOH_card_copy(ptrYugiohCard_Current, ptrYugiohCard_Main);
-//			RFID_Clear_Card_Bufffer(ptrRFID);
-//			ptrRFID->action = 1;
-//			break;
-//		case 1:
-//			/* Switch Card Type
-//			 * 		- 0 = Monster
-//			 * 		- 1 = Spell
-//			 * 		- 2 = Trap
-//			 *  */
-//			switch (ptrYugiohCard_Main->cardType)
-//			{
-//				case 0:
-//					break;
-//				case 1:
-//					break;
-//				case 2:
-//					break;
-//			}
-//			break;
-//		case 2:
-//			break;
-//	}
+	if (ptrRFID->action == 0) // Main Card is not assign
+	{
+		YUGIOH_card_copy(ptrYugiohCard_Current, ptrYugiohCard_Main);
+		RFID_Clear_Card_Bufffer(ptrRFID);
+		ptrRFID->action = 1;
+	}
+	else if (ptrRFID->action == 1)
+	{
+		/* Card Type
+		 * 		- 0 = Monster
+		 * 		- 1 = Spell
+		 * 		- 2 = Trap
+		 *  */
+		if (ptrYugiohCard_Main->cardType == 0 || ptrYugiohCard_Main->cardType == 1)
+		{
+			if (ptrYugiohCard_Current->cardData == ptrYugiohCard_Main->cardData)
+			{
+				ptrYugiohCard_Main->cardState = 1;
+				RFID_Clear_Card_Bufffer(ptrRFID);
+				ptrRFID->action = 2;
+			}
+			else
+			{
+				ptrRFID->action = 255;
+			}
+		}
+		else if (ptrYugiohCard_Main->cardType == 2)
+		{
+			ptrRFID->action = 2;
+		}
+	}
+	else if (ptrRFID->action == 2)
+	{
+		// AFK
+	}
+	else if (ptrRFID->action == 255)
+	{
+		RFID_Clear_Card_Bufffer(ptrRFID);
+		YUGIOH_Card buffCard = {0};
+		YUGIOH_card_copy(&buffCard, ptrYugiohCard_Main);
+		ptrRFID->action = 0;
+	}
 }
