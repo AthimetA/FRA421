c765f5eee1762513ebc7abf84610d2dac821504a
Athimet Aiewcharoen <mobiltiy2604@gmail.com>
2023-01-06 10:38:44 +0700

    re arange some code

--- Project/Common/Src/FRA421_Yugioh.c
+++ Project/Common/Src/FRA421_Yugioh.c
@@ -27,78 +27,6 @@ uint16_t timeinit = 600;
 uint16_t time = 0;
 uint8_t turn = 0;
 
-// RFID Handle Function
-void RFID_Main_init(RFIDHandle *rfidmain) {
-	RFID *ptrRFID = rfidmain->RFID;
-
-	for (uint8_t i = 0; i < RFID_NUM_MAX; ++i) {
-		ptrRFID = &rfidmain->RFID[i];
-		ptrRFID->slaveAddr = i;
-		ptrRFID->status = 0;
-		ptrRFID->action = 0;
-		RFID_Clear_Card_Bufffer(ptrRFID);
-	}
-}
-
-void YUGIOH_card_copy(YUGIOH_Card *src, YUGIOH_Card *dst) {
-	dst->cardData = src->cardData;
-	dst->cardSignature = src->cardSignature;
-	dst->cardState = src->cardState;
-	dst->cardType = src->cardType;
-	dst->cardLevel = src->cardLevel;
-	dst->cardAtk = src->cardAtk;
-	dst->cardDef = src->cardDef;
-	dst->standPosition = src->standPosition;
-	dst->targetPosition = src->targetPosition;
-	dst->actionPoint_Atk = src->actionPoint_Atk;
-	dst->actionPoint_Eff = src->actionPoint_Eff;
-}
-
-void YUGIOH_card_clear(YUGIOH_Card *card)
-{
-	YUGIOH_Card buff = {0};
-	YUGIOH_card_copy(&buff, card);
-}
-
-void RFID_Clear_Card_Bufffer(RFID *rfid) {
-	YUGIOH_Card *ptrYUGIOHCard = rfid->bufferCard;
-	for (uint8_t i = 0; i < CARD_BUFF_LEN; ++i) {
-		YUGIOH_card_clear(ptrYUGIOHCard);
-		ptrYUGIOHCard++;
-	}
-}
-
-void YUGIOH_card_register(RFIDHandle *rfidmain) {
-	RFID *ptrRFID = rfidmain->RFID;
-	Fra421_Card *ptrCard;
-	YUGIOH_Card *ptrYUGIOHCard;
-	ptrRFID = &rfidmain->RFID[rfidmain->slaveNum];
-	ptrCard = &ptrRFID->detectedCard;
-	ptrYUGIOHCard = ptrRFID->bufferCard;
-	// For now fix number
-	ptrYUGIOHCard->cardData = ptrCard->data;
-	// Load Data from Hash
-	YUGIOH_card_Load_Data(ptrYUGIOHCard);
-	ptrRFID->action = 1;
-	ptrYUGIOHCard->standPosition = ptrRFID->slaveAddr;
-
-}
-void YUGIOH_card_Buffer_Update(RFIDHandle *rfidmain) {
-	// Assign RFID
-	RFID *ptrRFID = rfidmain->RFID;
-	ptrRFID = &rfidmain->RFID[rfidmain->slaveNum];
-	// Buffer Card src
-	YUGIOH_Card *ptrYugiohCard_Buffer_src = ptrRFID->bufferCard;
-	ptrYugiohCard_Buffer_src = &ptrRFID->bufferCard[CARD_BUFF_LEN - 2];
-	// Buffer Card dst
-	YUGIOH_Card *ptrYugiohCard_Buffer_dst = ptrRFID->bufferCard;
-	ptrYugiohCard_Buffer_dst = &ptrRFID->bufferCard[CARD_BUFF_LEN - 1];
-	for (int i = CARD_BUFF_LEN; i >= 1; i--) {
-		YUGIOH_card_copy(ptrYugiohCard_Buffer_src, ptrYugiohCard_Buffer_dst);
-		ptrYugiohCard_Buffer_src--;
-		ptrYugiohCard_Buffer_dst--;
-	}
-}
 // Card Hash Function
 const uint8_t archive_yugioh_card_sig[256] = { 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0,
 		16, 0, 0, 0, 13, 17, 8, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 18,
@@ -171,6 +99,79 @@ const uint8_t archive_yugioh_card_def[256] = { 0, 0, 0, 0, 0, 0, 0, 0, 12, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 0, 14, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 14, 0, 0,
 		0, 0, 0, 0, 0, 21, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0,
 		19, 0, 0, 0, 0, 12, 0, 0, 0, };
+
+// RFID Handle Function
+void RFID_Main_init(RFIDHandle *rfidmain) {
+	RFID *ptrRFID = rfidmain->RFID;
+
+	for (uint8_t i = 0; i < RFID_NUM_MAX; ++i) {
+		ptrRFID = &rfidmain->RFID[i];
+		ptrRFID->slaveAddr = i;
+		ptrRFID->status = 0;
+		ptrRFID->action = 0;
+		RFID_Clear_Card_Bufffer(ptrRFID);
+	}
+}
+
+void YUGIOH_card_copy(YUGIOH_Card *src, YUGIOH_Card *dst) {
+	dst->cardData = src->cardData;
+	dst->cardSignature = src->cardSignature;
+	dst->cardState = src->cardState;
+	dst->cardType = src->cardType;
+	dst->cardLevel = src->cardLevel;
+	dst->cardAtk = src->cardAtk;
+	dst->cardDef = src->cardDef;
+	dst->standPosition = src->standPosition;
+	dst->targetPosition = src->targetPosition;
+	dst->actionPoint_Atk = src->actionPoint_Atk;
+	dst->actionPoint_Eff = src->actionPoint_Eff;
+}
+
+void YUGIOH_card_clear(YUGIOH_Card *card)
+{
+	YUGIOH_Card buff = {0};
+	YUGIOH_card_copy(&buff, card);
+}
+
+void RFID_Clear_Card_Bufffer(RFID *rfid) {
+	YUGIOH_Card *ptrYUGIOHCard = rfid->bufferCard;
+	for (uint8_t i = 0; i < CARD_BUFF_LEN; ++i) {
+		YUGIOH_card_clear(ptrYUGIOHCard);
+		ptrYUGIOHCard++;
+	}
+}
+
+void YUGIOH_card_register(RFIDHandle *rfidmain) {
+	RFID *ptrRFID = rfidmain->RFID;
+	Fra421_Card *ptrCard;
+	YUGIOH_Card *ptrYUGIOHCard;
+	ptrRFID = &rfidmain->RFID[rfidmain->slaveNum];
+	ptrCard = &ptrRFID->detectedCard;
+	ptrYUGIOHCard = ptrRFID->bufferCard;
+	// For now fix number
+	ptrYUGIOHCard->cardData = ptrCard->data;
+	// Load Data from Hash
+	YUGIOH_card_Load_Data(ptrYUGIOHCard);
+	ptrRFID->action = 1;
+	ptrYUGIOHCard->standPosition = ptrRFID->slaveAddr;
+
+}
+void YUGIOH_card_Buffer_Update(RFIDHandle *rfidmain) {
+	// Assign RFID
+	RFID *ptrRFID = rfidmain->RFID;
+	ptrRFID = &rfidmain->RFID[rfidmain->slaveNum];
+	// Buffer Card src
+	YUGIOH_Card *ptrYugiohCard_Buffer_src = ptrRFID->bufferCard;
+	ptrYugiohCard_Buffer_src = &ptrRFID->bufferCard[CARD_BUFF_LEN - 2];
+	// Buffer Card dst
+	YUGIOH_Card *ptrYugiohCard_Buffer_dst = ptrRFID->bufferCard;
+	ptrYugiohCard_Buffer_dst = &ptrRFID->bufferCard[CARD_BUFF_LEN - 1];
+	for (int i = CARD_BUFF_LEN; i >= 1; i--) {
+		YUGIOH_card_copy(ptrYugiohCard_Buffer_src, ptrYugiohCard_Buffer_dst);
+		ptrYugiohCard_Buffer_src--;
+		ptrYugiohCard_Buffer_dst--;
+	}
+}
 
 uint8_t CardHash_Encode(uint32_t key) {
 	key = ((key >> 16) ^ key) * 0x45d9f3b;
